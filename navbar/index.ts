7725b92fafbfadb020f8eaca0e2b42eca698b3f4
wb_dengwei05 <wb_dengwei05@meituan.com>
2024-03-19 19:57:23 +0800

    feat: update navbar

--- navbar/index.ts
+++ navbar/index.ts
@@ -1,15 +1,19 @@
 export default [
-  { text: '首页', link: '/' },
-  {
-    text: '前端技术栈',
-    items: [
-      { text: 'Vue', link: '/Frontend/' }
-    ]
-  },
-  {
-    text: '后端技术栈',
-    items: [
-      { text: 'MySQL', link: '/Backend/MySQL' }
-    ]
-  }
-]
+    { text: '首页', link: '/' },
+    {
+        text: '前端技术栈',
+        items: [{ text: 'Vue', link: '/Frontend/' }]
+    },
+    {
+        text: '后端技术栈',
+        items: [{ text: 'MySQL', link: '/Backend/MySQL' }]
+    },
+    {
+        text: '代码人生',
+        link: '/CodeLife/'
+    },
+    {
+        text: '周记',
+        link: '/WeeklyReport/'
+    }
+];
--- sidebar/index.ts
+++ sidebar/index.ts
@@ -1,5 +1,5 @@
 export default {
-  '/Backend/': [
-    { text: 'MySQL', link: '/Backend/MySQL.md' }
-  ]
-}
+    '/Backend/': [{ text: 'MySQL', link: '/Backend/MySQL.md' }],
+    '/CodeLife/': [{ text: '概述', link: '/CodeLife/' }],
+    '/WeeklyReport/': [{ text: '概述', link: '/WeeklyReport/' }]
+};
