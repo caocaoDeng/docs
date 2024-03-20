ff46833192b5cc71ffa8cb99079447afa334e97e
wb_dengwei05 <wb_dengwei05@meituan.com>
2024-03-20 19:55:43 +0800

    feat add content

--- sidebar/index.ts
+++ sidebar/index.ts
@@ -1,5 +1,8 @@
 export default {
     '/Backend/': [{ text: 'MySQL', link: '/Backend/MySQL.md' }],
-    '/CodeLife/': [{ text: '概述', link: '/CodeLife/' }],
+    '/CodeLife/': [
+        { text: '概述', link: '/CodeLife/' },
+        { text: '美团', link: '/CodeLife/meituan/meituan' }
+    ],
     '/WeeklyReport/': [{ text: '概述', link: '/WeeklyReport/' }]
 };
