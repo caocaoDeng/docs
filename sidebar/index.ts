a83eb299c49d6b0a7389966afebbae088e85d584
Deng`·caoaco <dengcaocao.dcc@gamil.com>
2024-03-31 20:26:10 +0800

    update docs

--- sidebar/index.ts
+++ sidebar/index.ts
@@ -3,6 +3,7 @@ export default {
         { text: '介绍', link: '/Backend/MySQL/' },
         { text: '快速开始', link: '/Backend/MySQL/install' },
         { text: 'SELECT语句', link: '/Backend/MySQL/select' },
+        { text: '运算符', link: '/Backend/MySQL/operator' },
     ],
     '/CodeLife/': [
         { text: '概述', link: '/CodeLife/' },
